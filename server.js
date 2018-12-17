bf5efae520674fdbce79c8a0020554fc7bfe128a
jarrenpatao <jarrenpatao@gmail.com>
2018-12-16 16:16:29 -0800

    trying to push to heroku master

--- server.js
+++ server.js
@@ -11,3 +11,13 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// home.html to be set as Index.html
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
